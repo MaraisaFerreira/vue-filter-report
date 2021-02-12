9e8f501668624db6fa26bca45b24078394737ee4
maraisaferreira <ferreira.maraisacristina@gmail.com>
2021-02-11 21:51:21 -0300

    script update

---
+++ js/script.js
@@ -0,0 +1,53 @@
+Vue.directive('money', {
+	inserted(el, binding) {
+		const amount = parseFloat(el.innerHTML)
+			.toFixed(2)
+			.replace('.', ',')
+			.replace(/(\d)(?=(\d{3})+\,)/g, '$1.');
+		el.innerHTML = `${binding.value} ${amount}`;
+	},
+});
+
+new Vue({
+	el: '#app',
+	data: {
+		title: 'Relatório',
+		sales: null,
+		btnComplete: 'Lista Completa',
+		btnResume: 'Lista Resumida',
+	},
+	methods: {
+		getSales() {
+			fetch('/data/sales.json')
+				.then((resp) => resp.json())
+				.then((data) => (this.sales = data));
+		},
+		calculateTotal(sales) {
+			if (!sales) return 0;
+			return sales.reduce((total, sale) => total + sale.value, 0);
+		},
+	},
+	computed: {
+		salesByProduct() {
+			if (!this.sales) return null;
+
+			const salesByProduct = this.sales.reduce((grouped, sale) => {
+				if (!grouped.hasOwnProperty(sale.name)) {
+					grouped[sale.name] = {
+						name: sale.name,
+						value: sale.price * sale.qtde,
+					};
+					return grouped;
+				}
+
+				grouped[sale.name].value += sale.price * sale.qtde;
+				return grouped;
+			}, {});
+
+			return Object.values(salesByProduct);
+		},
+	},
+	mounted() {
+		this.getSales();
+	},
+});
